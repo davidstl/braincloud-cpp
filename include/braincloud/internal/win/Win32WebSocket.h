2d85edd06cd23ae94f42269f825b8128c135d6b3
davidstl <davidstl@bitheads.com>
2018-09-28 07:43:20 -0400

    Added new files

---
+++ include/braincloud/internal/win/Win32WebSocket.h
@@ -0,0 +1,58 @@
+#ifndef _WIN32WEBSOCKER_H_
+#define _WIN32WEBSOCKER_H_
+
+#include "braincloud/internal/IWebSocket.h"
+
+#include <atomic>
+#include <condition_variable>
+#include <map>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+#include <vector>
+
+namespace BrainCloud
+{
+    class Win32WebSocket : public IWebSocket
+    {
+    public:
+        virtual ~Win32WebSocket();
+
+        virtual bool isValid();
+
+        virtual void send(const std::string& message);
+        virtual std::string recv();
+
+        virtual void close();
+
+    protected:
+        friend class IWebSocket;
+
+        Win32WebSocket(const std::string& address, int port, const std::map<std::string, std::string>& headers);
+
+    private:
+        // State
+        bool _isValid;
+        bool _isConnecting;
+
+        // Update and send queue
+        std::mutex _mutex;
+        std::queue<std::string> _sendQueue;
+        std::vector<uint8_t> _sendBuffer;
+        std::thread _updateThread;
+
+        // Connection
+        std::condition_variable _connectionCondition;
+
+        // Receiving queue
+        std::mutex _recvMutex;
+        std::queue<std::string> _recvQueue;
+        std::condition_variable _recvCondition;
+
+        // Context
+        std::map<std::string, std::string> _authHeaders;
+    };
+};
+
+#endif /* _DEFAULTTCPSOCKER_H_ */
---
+++ src/win/Win32WebSocket.cpp
@@ -0,0 +1,91 @@
+#include "braincloud/internal/Win32WebSocket.h"
+
+namespace BrainCloud
+{
+    IWebSocket* IWebSocket::create(const std::string& address, int port, const std::map<std::string, std::string>& headers)
+    {
+        return new Win32WebSocket(address, port, headers);
+    }
+
+    Win32WebSocket::~Win32WebSocket()
+    {
+        close();
+    }
+
+    Win32WebSocket::Win32WebSocket(const std::string& uri, int port, const std::map<std::string, std::string>& headers)
+        : _isValid(false)
+        , _isConnecting(true)
+        , _authHeaders(headers)
+    {
+    }
+
+    bool Win32WebSocket::isValid()
+    {
+        return false;
+        // std::unique_lock<std::mutex> lock(_mutex);
+
+        // if (_isConnecting)
+        // {
+        //     _connectionCondition.wait(lock, [this]() { return !_isConnecting; });
+        // }
+
+        // return _isValid;
+    }
+
+    void Win32WebSocket::send(const std::string& message)
+    {
+        // std::unique_lock<std::mutex> lock(_mutex);
+        // _sendQueue.push(message);
+    }
+
+    std::string Win32WebSocket::recv()
+    {
+        // std::unique_lock<std::mutex> lock(_recvMutex);
+
+        // if (!_recvQueue.empty())
+        // {
+        //     std::string message = _recvQueue.front();
+        //     _recvQueue.pop();
+        //     return message;
+        // }
+
+        // _recvCondition.wait(lock, [this]()
+        // {
+        //     return !_isValid || !_recvQueue.empty();
+        // });
+
+        // if (_isValid && !_recvQueue.empty())
+        // {
+        //     std::string message = _recvQueue.front();
+        //     _recvQueue.pop();
+        //     return message;
+        // }
+
+        return "";
+    }
+
+    void Win32WebSocket::close()
+    {
+        // Stop and clean send queue
+        // {
+        //     std::unique_lock<std::mutex> lock(_mutex);
+        //     while (!_sendQueue.empty()) _sendQueue.pop();
+        //     _isValid = false;
+        //     _isConnecting = false;
+        //     _connectionCondition.notify_all();
+        // }
+
+        // // Stop and clean recving
+        // {
+        //     std::unique_lock<std::mutex> lock(_recvMutex);
+        //     while (!_recvQueue.empty()) _recvQueue.pop();
+        //     _recvCondition.notify_all();
+        // }
+
+        // // Join update thread
+        // if (_updateThread.joinable())
+        // {
+        //     _updateThread.join();
+        // }
+    }
+};
